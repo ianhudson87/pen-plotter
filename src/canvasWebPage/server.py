27eaf0ef48509c185738a11184f133ea96ed7912
Ian Hudson <hudsonian@wustl.edu>
2025-03-21 21:03:40 -0400

    server

--- src/canvasWebPage/server.py
+++ src/canvasWebPage/server.py
@@ -1,6 +1,10 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request
 import os
 import time
+import sys
+
+opStatusFilePath = sys.argv[1]
+gcodeFilePath = sys.argv[2]
 
 app = Flask(__name__)
 
@@ -13,9 +17,21 @@ def serve_html():
 
 @app.route('/plot', methods=['POST'])
 def plot():
-    print("Plotting!")
-    time.sleep(5)
-    return "Plotting request accepted", 202
+    print("plot requested")
+    print(request.json)
+    with open(opStatusFilePath, 'r+') as opStatusFile:
+        status = opStatusFile.read()
+        print(status)
+        if status == "waiting":
+            # write gcodefile
+            with open(gcodeFilePath, 'r+') as gcodeFile:
+                gcodeFile.write
+            opStatusFile.write("queued")
+            return "Plotting request accepted", 202
+            # return job accepted
+        else:
+            # another job is already queued. return conflict
+            return "Conflict. Job is already running", 409
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
